8ef428efa181999467f7be67f4bfe7af10f303c9
Andrew Kim <kim01344@umn.edu>
2025-05-01 12:49:09 -0500

    TABLENAME.sql to BOOK.sql

--- BOOK.sql
+++ BOOK.sql
@@ -1,5 +1,5 @@
-BOOK
-CREATE TABLE Book (
+
+CREATE TABLE BOOK (
   BookID           INT             PRIMARY KEY,
   Title            VARCHAR(255)    NOT NULL,
   Genre            VARCHAR(100),
@@ -8,13 +8,5 @@ CREATE TABLE Book (
   PublisherContact VARCHAR(100),
   StockQuantity    INT,
   Price            DECIMAL(10, 2)
-);
-
-AUTHOR
-CREATE TABLE Author (
-  AuthorID         INT             PRIMARY KEY,
-  FirstName        VARCHAR(100),
-  LastName         VARCHAR(100),
-  Biography        TEXT
 );
 
